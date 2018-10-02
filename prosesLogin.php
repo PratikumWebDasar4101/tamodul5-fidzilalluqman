f28d1ec215804cfabbd6f5be1e0956c5413ccdd4
fidzilalluqman <42725339+fidzilalluqman@users.noreply.github.com>
2018-10-02 20:24:11 +0700

    Update prosesLogin.php

--- prosesLogin.php
+++ prosesLogin.php
@@ -1,7 +1,16 @@
-<?php 
-
-
-
-
-
+<?php  
+session_start();
+error_reporting(0);
+$user = array(
+				"username" => "admin",
+				"password" => "admin"
+			);
+if (isset($_POST['submit'])) {
+	if ($_POST['username'] == $user['username'] && $_POST['password'] == $user['password']){
+		$_SESSION['username'] = $_POST['username'];
+		header("Location: registrasi.php");
+	}else{
+		echo "Username atau Password Tidak Benar";
+	}
+}
 ?>
