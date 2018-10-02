c3cfffdc0c69b8c73b115760bc8a76484f509384
fidzilalluqman <42725339+fidzilalluqman@users.noreply.github.com>
2018-10-02 20:23:35 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,24 @@
-<?php
-
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1 align="center">Login</h1>
+<table border="1" align="center">
+	<form action="proseslogin.php" method="POST">
+	<tr>
+		<th>Username</th>
+		<td><input type="text" name="username" id="username"></td>
+	</tr>
+	<tr>
+		<th>Password</th>
+		<td><input type="password" name="password" id="password"></td>
+	</tr>
+	<tr>
+		<td colspan="2"><input style="width: 100%" type="submit" name="submit"></td>
+	</tr>
+</form> 
+</table>
+</body>
+</html>
